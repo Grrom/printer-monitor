7d95b73df7af699ec238db1de0d923166da00552
Jerome <josejeromelalunio@gmail.com>
2021-08-27 07:00:06 +0800

    deploy

---
+++ precache-manifest.29d7acc5c65faa96a1102efa0c087aa4.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86f107245449784e5024",
+    "url": "/printer-monitor/css/app.3a0ba0d6.css"
+  },
+  {
+    "revision": "66484d66c7418b13a24c097a28b1f012",
+    "url": "/printer-monitor/img/close.66484d66.svg"
+  },
+  {
+    "revision": "10cb83cddac0be061683a26addf578a3",
+    "url": "/printer-monitor/img/details.10cb83cd.png"
+  },
+  {
+    "revision": "a3dc7c6f67d78f691038c4f355008d33",
+    "url": "/printer-monitor/img/download.a3dc7c6f.svg"
+  },
+  {
+    "revision": "12ea306b17fdb190b719b7d252c61e2c",
+    "url": "/printer-monitor/img/hamburger.12ea306b.svg"
+  },
+  {
+    "revision": "7c218a8940680dd1d742a85ffc41a2c4",
+    "url": "/printer-monitor/img/home-image.7c218a89.png"
+  },
+  {
+    "revision": "829f50172de423c1527d2548c2210143",
+    "url": "/printer-monitor/img/jobstatus.829f5017.png"
+  },
+  {
+    "revision": "eea6e88b47ecdf9ecaad4fa5a382f520",
+    "url": "/printer-monitor/img/play.eea6e88b.svg"
+  },
+  {
+    "revision": "a99c4a1367c68bf1073bb1f5509d330f",
+    "url": "/printer-monitor/img/state.a99c4a13.png"
+  },
+  {
+    "revision": "3a89077a2f5244663975515ebeee34c3",
+    "url": "/printer-monitor/img/timelapse.3a89077a.png"
+  },
+  {
+    "revision": "d78c19089e6edce55a3de715a07d855a",
+    "url": "/printer-monitor/index.html"
+  },
+  {
+    "revision": "86f107245449784e5024",
+    "url": "/printer-monitor/js/app.fa32821e.js"
+  },
+  {
+    "revision": "264510384125bb38dd71",
+    "url": "/printer-monitor/js/chunk-vendors.84d0d683.js"
+  },
+  {
+    "revision": "0a0df928ef6e8e95000afaee7aa72806",
+    "url": "/printer-monitor/manifest.json"
+  },
+  {
+    "revision": "60078a0ba512b85a1a6642ab24cd68c4",
+    "url": "/printer-monitor/media/groot.60078a0b.mp4"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/printer-monitor/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/printer-monitor/precache-manifest.29d7acc5c65faa96a1102efa0c087aa4.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "printer-monitoring"});
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
