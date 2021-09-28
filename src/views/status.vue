b6f352ccb2f5087a7def22864f5371bc6084c811
gino <you@email.com>
2021-09-28 12:18:18 +0900

    clean up

--- src/views/status.vue
+++ src/views/status.vue
@@ -74,8 +74,6 @@ export default defineComponent({
 
     const jobStatus = ref({ job: "", state: "" });
 
-    //const printing = ref(jobStatus.value.state == "printing");
-
     const printing = computed(() => {
       return jobStatus.value.state;
     });
@@ -89,9 +87,7 @@ export default defineComponent({
 
     function togglePrint() {
       if (printing.value == "Paused" || printing.value == "Printing") {
-        let command: string = printing.value == "Printing" ? "resume" : "pause";
-
-        fetch("http://192.168.43.60/api/job", {
+        fetch(ip + "job", {
           headers: {
             accept: "application/json, text/javascript, */*; q=0.01",
             "accept-language": "en-US,en;q=0.9",
@@ -103,45 +99,13 @@ export default defineComponent({
           body: '{"action":"toggle","command":"pause"}',
           method: "POST",
         });
-
-        // let formData = new FormData();
-        // formData.append("command", "pause");
-        // formData.append("action", "toggle");
-        // fetch(`${ip}job`, {
-        //   method: "POST",
-        //   body: formData,
-        //   headers: {
-        //     "content-type": "application/json",
-        //     "X-Api-Key": apiKey,
-        //   },
-        // })
-        //   .then(function (response) {
-        //     return response.json();
-        //   })
-        //   .then(function (data) {
-        //     console.log(data);
-        //   })
-        //   .catch(function (err) {
-        //     console.warn("Something went wrong.", err);
-        //   });
       } else {
-        window.alert("there is no job queued");
+        window.alert(
+          printing.value == "Pausing"
+            ? "Can't issue a command while the printer is Pausing, please wait"
+            : "there is no job queued"
+        );
       }
-
-      //     fetch("http://192.168.43.60/api/job", {
-      //   "headers": {
-      //     "accept": "application/json, text/javascript, */*; q=0.01",
-      //     "accept-language": "en-US,en;q=0.9",
-      //     "cache-control": "no-cache",
-      //     "content-type": "application/json; charset=UTF-8",
-      //     "x-requested-with": "XMLHttpRequest"
-      //   },
-      //   "referrerPolicy": "no-referrer",
-      //   "body": "{\"action\":\"resume\",\"command\":\"pause\"}",
-      //   "method": "POST",
-      //   "mode": "cors",
-      //   "credentials": "include"
-      // });
     }
 
     const printTime = computed(() => formatTime(progress.value.printTime));
@@ -171,50 +135,6 @@ export default defineComponent({
         });
     }
 
-    // http://192.168.43.60/api/job
-    // POST /api/job HTTP/1.1
-    // Host: 192.168.43.60
-    // Connection: keep-alive
-    // Content-Length: 36
-    // Accept: application/json, text/javascript, */*; q=0.01
-    // Cache-Control: no-cache
-    // X-Requested-With: XMLHttpRequest
-    // User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.54 Safari/537.36
-    // Content-Type: application/json; charset=UTF-8
-    // Origin: http://192.168.43.60
-    // Accept-Encoding: gzip, deflate
-    // Accept-Language: en-US,en;q=0.9
-    // Cookie: remember_token_P80=3DPrinting|83bb21da93d57b472552fa503231c3635b8d1f7446d16677b1a5b3d496a8a5f972feafdf91c2d4e9cd83a9e10107d29d4c26f51296b1c17e4d2c0e2e305aaf49; session_P80=.eJxljktqAzEQRO-idQjqbkmtnp3HOYBvMOjTigfMOMxnYUzuHgVvAtlV1aOKepqprbpdzbCvh76Zaa5mME5UvC0Yhbxr5HMBYgsxR0sSbJCamlWJmFPjIgBNW-OWwNfIGTFyJE4ZSwkIAWKlHEgZIAVIvUasyuIUEJsPqdsai-YSnK9VOJl-5Nh0fb2hj8s6L_u8fPZ8rtrl_nhPx36d9seXmmE5brc_5H_pd2vTbZvvy4vKyONZrI_uRO48UpfhxAiM2D0F8_0DMONS7g.YVEsoQ.uLiWev2GkGcFixVeujt_kd5Nmcs
-
-    // fetch("http://192.168.43.60/api/job", {
-    //   "headers": {
-    //     "accept": "application/json, text/javascript, */*; q=0.01",
-    //     "accept-language": "en-US,en;q=0.9",
-    //     "cache-control": "no-cache",
-    //     "content-type": "application/json; charset=UTF-8",
-    //     "x-requested-with": "XMLHttpRequest"
-    //   },
-    //   "referrerPolicy": "no-referrer",
-    //   "body": "{\"action\":\"pause\",\"command\":\"pause\"}",
-    //   "method": "POST",
-    //   "mode": "cors",
-    //   "credentials": "include"
-    // });
-
-    //     fetch("http://192.168.43.60/api/job", {
-    //   "headers": {
-    //     "accept": "application/json, text/javascript, */*; q=0.01",
-    //     "accept-language": "en-US,en;q=0.9",
-    //     "cache-control": "no-cache",
-    //     "content-type": "application/json; charset=UTF-8",
-    //     "x-requested-with": "XMLHttpRequest"
-    //   },
-    //   "referrerPolicy": "no-referrer",
-    //   "body": "{\"action\":\"resume\",\"command\":\"pause\"}",
-    //   "method": "POST",
-    //   "mode": "cors",
-    //   "credentials": "include"
-    // });
     onMounted(() => {
       setInterval(requestStatus, 1000);
       // requestStatus();
